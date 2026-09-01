a6e7d597c67f522c90d1496bde05a5197ee7e8a7
aarav-raghu <Aarav.raghu@icloud.com>
2026-09-01 09:42:17 +0100

    Subtracting the same number

--- hello.py
+++ hello.py
@@ -7,3 +7,8 @@ def calculate_add(number:int):
     return number + number
 
 print(calculate_add(4444))
+
+def calculate_subtract(number:int):
+    return number - number
+
+print(calculate_subtract(6764))
